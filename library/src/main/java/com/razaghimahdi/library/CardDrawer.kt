739d9b6c422afeeafb682c1fec4c514e98e0d3a3
razaghimahdi <razaghimahdi78@gmail.com>
2023-04-08 12:31:14 +0330

    - Added CardDrawer.kt

---
+++ library/src/main/java/com/razaghimahdi/library/CardDrawer.kt
@@ -0,0 +1,259 @@
+/*
+ * Copyright (C) 2023 razaghimahdi (Mahdi Razzaghi Ghaleh)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.razaghimahdi.library
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.semantics.dismiss
+import androidx.compose.ui.semantics.paneTitle
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import com.razaghimahdi.library.core.*
+import com.razaghimahdi.library.core.toDp
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
+
+
+/**
+ * @param drawerContent composable that represents content inside the drawer
+ * @param modifier optional modifier for the drawer
+ * @param drawerState state of the drawer
+ * @param gesturesEnabled whether or not drawer can be interacted by gestures
+ * @param drawerBackgroundColor background color to be used for the drawer sheet
+ * @param drawerContentColor color of the content to use inside the drawer sheet. Defaults to
+ * @param contentCornerSize size of shape of the content
+ * @param contentBackgroundColor background color to be used for the content
+ * @param content content of the rest of the UI
+ *
+ * @throws IllegalStateException when parent has [Float.POSITIVE_INFINITY] width
+ */
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun CardDrawer(
+    drawerContent: @Composable ColumnScope.() -> Unit,
+    modifier: Modifier = Modifier,
+    drawerState: CardDrawerState =
+        rememberCardDrawerState(CardDrawerValue.Closed),
+    gesturesEnabled: Boolean = true,
+    drawerBackgroundColor: Color = MaterialTheme.colors.surface,
+    drawerContentColor: Color = contentColorFor(drawerBackgroundColor),
+    contentCornerSize: Dp = 0.dp,
+    contentBackgroundColor: Color = MaterialTheme.colors.surface,
+    content: @Composable () -> Unit
+) {
+    val scope = rememberCoroutineScope()
+
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp.toPx()
+    val screenWidth = configuration.screenWidthDp.dp.toPx()
+
+
+    val startContentXPosition = 0f
+    val endContentXPosition = screenWidth / 1.4f
+
+    val startDrawerYRotation = 0f
+    val endDrawerYRotation = -25f
+
+    val startDrawerXPosition = 0f
+    val endDrawerXPosition = -screenWidth
+
+/*
+    var currentCornerShapeContent by remember {
+        mutableStateOf(RoundedCornerShape(contentCornerSize))
+    }
+
+
+    currentCornerShapeContent = if (contentCornerSize != 0.dp) {
+        RoundedCornerShape((((contentCornerSize.value * (drawerState.offset.value / screenWidth)) + contentCornerSize.value).roundToInt().dp))
+        RoundedCornerShape(contentCornerSize)
+    } else {
+        RoundedCornerShape(
+            0.dp
+        )
+    }
+    */
+
+/*
+
+    var drawerYRotation by remember { mutableStateOf(startDrawerYRotation) }
+    val contentXPosition = remember { Animatable(startContentXPosition) }
+    val drawerXPosition = remember { Animatable(startDrawerXPosition) }
+
+    if (drawerState.currentValue == DrawerLayoutValue.Open) {
+
+        LaunchedEffect(Unit) {
+            launch {
+                contentXPosition.animateTo(
+                    endContentXPosition,
+                    animationSpec = drawerState.AnimationSpec,
+                )
+            }
+
+            launch {
+                drawerXPosition.animateTo(
+                    startDrawerXPosition,
+                    animationSpec = drawerState.AnimationSpec,
+                )
+            }
+
+            launch {
+                animate(
+                    initialValue = startDrawerYRotation,
+                    targetValue = endDrawerYRotation,
+                    animationSpec = drawerState.AnimationSpec,
+                ) { value: Float, _: Float ->
+                    drawerYRotation = value
+                }
+            }
+        }
+
+    } else {
+
+        LaunchedEffect(Unit) {
+            launch {
+                contentXPosition.animateTo(
+                    startContentXPosition,
+                    animationSpec = drawerState.AnimationSpec,
+                )
+            }
+
+            launch {
+                drawerXPosition.animateTo(
+                    endDrawerXPosition,
+                    animationSpec = drawerState.AnimationSpec,
+                )
+            }
+
+            launch {
+                animate(
+                    initialValue = endDrawerYRotation,
+                    targetValue = startDrawerYRotation,
+                    animationSpec = drawerState.AnimationSpec,
+                ) { value: Float, _: Float ->
+                    drawerYRotation = value
+                }
+            }
+        }
+
+    }
+
+*/
+
+
+    BoxWithConstraints(modifier.background(drawerBackgroundColor)) {
+        val modalDrawerConstraints = constraints
+        // TODO : think about Infinite max bounds case
+        if (!modalDrawerConstraints.hasBoundedWidth) {
+            throw IllegalStateException("Drawer shouldn't have infinite width")
+        }
+
+
+        val minValue = -modalDrawerConstraints.maxWidth.toFloat()
+        val maxValue = 0f
+        val anchors =
+            mapOf(minValue to CardDrawerValue.Closed, maxValue to CardDrawerValue.Open)
+        val isRtl = LocalLayoutDirection.current == LayoutDirection.Rtl
+        Box(
+            Modifier.swipeable(
+                state = drawerState.swipeableState,
+                anchors = anchors,
+                thresholds = { _, _ -> FractionalThreshold(0.5f) },
+                orientation = Orientation.Horizontal,
+                enabled = gesturesEnabled,
+                reverseDirection = isRtl,
+                velocityThreshold = drawerState.DrawerVelocityThreshold,
+                resistance = null
+            )
+        ) {
+
+            Surface(
+                Modifier
+                    .fillMaxHeight()
+                    .offset {
+                        // IntOffset(drawerState.offset.value.roundToInt(), 0)
+
+                        val x = (drawerState.offset.value.roundToInt())
+                        if (x <= (screenWidth / 1.4f).roundToInt()) {
+                            IntOffset(x, 0)
+                        } else {
+                            IntOffset((screenWidth / 1.4f).roundToInt(), 0)
+                        }
+
+
+                    }
+                    // .offset(x = drawerXPosition.value.toDp())
+                    .width(endContentXPosition.toDp())
+                    .widthIn(0.dp, endContentXPosition.toDp())
+                    .semantics {
+                        paneTitle = "FullDrawerLayout"
+                        if (drawerState.isOpen) {
+                            dismiss {
+                                if (
+                                    drawerState.currentValue == CardDrawerValue.Closed
+                                ) {
+                                    scope.launch { drawerState.close() }
+                                }; true
+                            }
+                        }
+                    },
+                color = drawerBackgroundColor,
+                contentColor = drawerContentColor
+            ) {
+                Column(content = drawerContent)
+            }
+
+
+
+            Surface(
+                Modifier
+                    .fillMaxSize()
+                    .offset {
+                        val x = (drawerState.offset.value.roundToInt() + screenWidth.roundToInt())
+                        if (x <= (screenWidth / 1.4f).roundToInt()) {
+                            IntOffset(x, 0)
+                        } else {
+                            IntOffset((screenWidth / 1.4f).roundToInt(), 0)
+                        }
+                    }
+                    //.offset(x = contentXPosition.value.toDp())
+                    .graphicsLayer {
+                        // rotationY = if(drawerState.isClosed) 0f else  (1 - (drawerState.offset.value / (screenWidth / 1.4f))) * -45f
+                        rotationY = (-((25 * (drawerState.offset.value / screenWidth)) + 25))
+                        // rotationY = drawerYRotation
+                        // cameraDistance = screenWidth + screenHeight
+                        cameraDistance = 8 * density
+                    },
+                color = contentBackgroundColor,
+                shape = RoundedCornerShape((((contentCornerSize.value * (drawerState.offset.value / screenWidth)) + contentCornerSize.value).roundToInt().dp))
+            ) {
+                content()
+            }
+
+        }
+    }
+}
